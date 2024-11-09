8079471c886309fe941c60d0db5b0284aec6e903
KTCume <167701720+KTCume@users.noreply.github.com>
2024-11-09 12:36:49 +0900

    P73から

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { News } from "@/app/_libs/microcms";
 
-
-type News = {
-  id: string;
-  title: string;
-  category: {
-    name: string
-  };
-  publishedAt: string;
-  createdAt: string;
-};
 
 const data: {
   contents: News[] } = {
@@ -48,7 +41,9 @@ const data: {
   };
 
 export default function Home() {
-   const sliceData = data.contents.slice(0, 2);
+   // const sliceData = data.contents.slice(0, 2);
+  const sliceData: News = [];
+
   return(
     <>
     <section className={styles.top}>
@@ -68,39 +63,10 @@ export default function Home() {
     </section>
     <section className={styles.news}>
       <h2 className={styles.newsTitle}>News</h2>
-      <ul>
-        {sliceData.map((article) =>(
-          <li key={article.id} className={styles.list}>
-            <div className={styles.link}>
-              <Image
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
-              />
-              <dl className={styles.contents}>
-                <dt className={styles.newItemTitle}>
-                  {article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.tag}>
-                    {article.category.name}</span>
-                    <span className={styles.gate}>
-                      <Image
-                      src="/clock.svg"
-                      alt=""
-                      width={16}
-                      height={16}
-                      priority
-                      />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-              </dl>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={sliceData} />
+      <div className={styles.newsLink}>
+        <ButtonLink href="/news">もっとみる</ButtonLink>
+      </div>
     </section>
     </>
   ); 
