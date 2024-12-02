c0872b56ef275f7758fd0c88e4b0e8504efea548
KTCume <167701720+KTCume@users.noreply.github.com>
2024-12-02 20:18:55 +0900

    P101

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,4 +1,5 @@
-import { profile } from "console";
+import Image from "next/image";
+import styles from "./page.module.css"
 
 const data = {
     contents: [
@@ -44,7 +45,7 @@ const data = {
 export default function Page() {
     return (
         <div className={styles.container}>
-            {data.contents.lenght === 0 ? (
+            {data.contents.length === 0 ? (
                 <p className={styles.empty}>メンバーが登録されていません。</p>
             ) : (
                 <ul>
@@ -52,6 +53,16 @@ export default function Page() {
                         <li key={member.id} className={styles.list}>
                             <Image
                             src={member.image.url}
+                            alt=""
+                            width={member.image.width}
+                            height={member.image.height}
+                            className={styles.image}
+                            />
+                            <dl>
+                            <dt className={styles.name}>{member.name}</dt>
+                            <dd className={styles.position}>{member.position}</dd>
+                            <dd className={styles.profile}>{member.profile}</dd>
+                            </dl>
                         </li>
                     ))}
                 </ul>
