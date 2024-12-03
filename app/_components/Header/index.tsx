80a276d20aee8868b5e0308b35cdebdef0bb23df
KTCume <167701720+KTCume@users.noreply.github.com>
2024-12-03 17:40:17 +0900

    ５章から

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import styles  from "./index.module.css";
 import Menu from "../Menu";
 
-export  default function Header() {
+export default function Header() {
     return (
         <header className={styles.header}>
-            <Link href="/" className={styles.header}>
+            <Link href="/" className={styles.logoLink}>
             <Image
             src="/logo.svg"
             alt="SIMPLE"
