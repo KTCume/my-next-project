3bbeada3045361ceb796903a30fe795b2261ee52
KTCume <167701720+KTCume@users.noreply.github.com>
2024-12-03 15:00:10 +0900

    P112

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./index.module.css";
 
 export default function Menu() {
+    const open = () => {
+        document.querySelector
+    }
     return (
         <div>
         <nav className={styles.nav}>
@@ -18,7 +23,9 @@ export default function Menu() {
                 </li>
             </ul>
         </nav>
-        <button className={styles}
+        <button className={styles.button}>
+            <Image src="/menu.svg" alt="メニュー" width={24} height={24}/>
+        </button>
         </div>
     );
 }
