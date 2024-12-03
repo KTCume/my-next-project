4dbc5d334753f3f96df0497d2d61e0d7a50c3bc9
KTCume <167701720+KTCume@users.noreply.github.com>
2024-12-03 16:58:45 +0900

    p114

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -2,15 +2,17 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
+import cx from "classnames";
 import styles from "./index.module.css";
 
 export default function Menu() {
-    const open = () => {
-        document.querySelector
-    }
+    const [isOpen, setOpen] = useState<boolean>(false);
+    const open = () => setOpen(true);
+
     return (
         <div>
-        <nav className={styles.nav}>
+        <nav className={cx(styles.nav, isOpen && styles.open)}>
             <ul className={styles.items}>
                 <li>
                     <Link href="/news">ニュース</Link>
@@ -23,7 +25,7 @@ export default function Menu() {
                 </li>
             </ul>
         </nav>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={open}>
             <Image src="/menu.svg" alt="メニュー" width={24} height={24}/>
         </button>
         </div>
