d5c4c173942df75e70164402b931e1e8a3278cf6
KTCume <167701720+KTCume@users.noreply.github.com>
2024-12-03 14:47:14 +0900

    P110

---
+++ app/_components/Menu/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./index.module.css";
+
+export default function Menu() {
+    return (
+        <div>
+        <nav className={styles.nav}>
+            <ul className={styles.items}>
+                <li>
+                    <Link href="/news">ニュース</Link>
+                </li>
+                <li>
+                    <Link href="/member">メンバー</Link>
+                </li>
+                <li>
+                    <Link href="/contact">お問い合わせ</Link>
+                </li>
+            </ul>
+        </nav>
+        <button className={styles}
+        </div>
+    );
+}
