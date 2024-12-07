22093d48d41683c77af1823a4c0cf05b59e7af2d
KTCume <167701720+KTCume@users.noreply.github.com>
2024-12-07 17:14:44 +0900

    サイト全体にrevalidateを設定する

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
 
 
-
-
 export default async function Home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
